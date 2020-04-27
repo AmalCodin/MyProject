5fec2b5163c0f148e62268bec527bea37adc9012
Amal Shaji <amalshaji663123@gmail.com>
2020-04-27 12:13:39 +0400

    A Program That Can Choose Runners For A Race

--- sandbox.js
+++ sandbox.js
@@ -1,43 +1,16 @@
-//learning to us functions
+let raceNumber = Math.floor(Math.random() * 1000);
 
-function greet(){
-    console.log('hello there');
+const runnerRegistered = true;
+
+const runnerAge = 10;
+
+if(runnerAge > 18 && runnerRegistered) {
+  raceNumber += 1000
+  console.log(`You will be race at 9:30 am ${raceNumber} is your race number.`);
+} else if(runnerAge > 18 && !runnerRegistered) {
+  console.log(`You will race at 11:00 am ${raceNumber} is your race number.`)
+} else if(runnerAge < 18) {
+  console.log(`You will race at 12:30 pm ${raceNumber} is your race number.`)
+} else {
+  console.log('You should see the registration desk.')
 }
-
-//result of using the function or calling out the function.
-
-/*greet()
-greet()
-greet()*/
-
-const speak = function(){
-    console.log('good day!')
-}
-
-speak()
-
-//arguments and parameter
-
-const talk = function(name){
-    console.log(`HAVE AN AWESOME DAY ${name}`)
-}
-
-talk('Amal')
-
-//two parameters
-
-const timeday = function(time, day){
-    console.log(`It is lovely in ${time} have an awesome ${day} pal`)
-}
-
-timeday('2 o clock', 'Friday')
-
-
-//function to calculate the area
-
-const CalcArea = function(radius){{
-    let area = 3.14 * radius**2;
-    console.log(area)
-}}
-
-CalcArea(14)
